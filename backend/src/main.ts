74d9866a5648c4e460826a35202bb22bad06e400
goldsami <zolotarenkomax@gmail.com>
2021-10-11 22:26:20 +0300

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
-  console.log(`Server has been started on port ${PORT}...`);
+  console.log(`Server has been started o n   port ${PORT}...`);
 }
 bootstrap();
